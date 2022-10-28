663c2351d0fd50ef780df5b375c6a8d67804004a
kovasah <samkovacevic05@gmail.com>
2022-10-28 11:20:35 +0200

    json request

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("hehe");
+fetch('https://jsonplaceholder.typicode.com/todos')
+  .then(response => response.json())
+  .then(json =>
+    json.forEach((e) => {
+        console.log(e);
+    })
+  );
